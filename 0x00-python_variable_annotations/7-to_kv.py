451e5135ce41a75af566aa497ef19a397c764579
Lawremwashail <lawremwash@gmail>
2024-10-12 02:11:53 +0300

    7-tokv

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Type-annotated function"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[str, float]) -> Tuple[str, float]:
+    """Function that returns a tuple of a str and float """
+    a = v ** 2
+    return (k, a)
